27ae1977e77fef9834fc2216cf4860f855ffbe0a
bego <begonapm@ongres.com>
2019-07-04 16:14:03 +0200

    Change to static method

--- saslprep/src/test/java/org/sasl/prep/SaslPrepTest.java
+++ saslprep/src/test/java/org/sasl/prep/SaslPrepTest.java
@@ -10,33 +10,32 @@ import org.junit.Test;
 public class SaslPrepTest {
   @Test
   public void examplesRtf() throws IOException {
-    SaslPrep saslPrep = new SaslPrep();
     String example1 = "I\u00ADX";
-    assertEquals("IX", saslPrep.saslPrep(example1, false));
+    assertEquals("IX", SaslPrep.saslPrep(example1, true));
     String example2 = "user";
-    assertEquals(example2, saslPrep.saslPrep(example2, false));
+    assertEquals(example2, SaslPrep.saslPrep(example2, true));
     String example3 = "USER";
-    assertEquals(example3, saslPrep.saslPrep(example3, false));
+    assertEquals(example3, SaslPrep.saslPrep(example3, true));
     String example4 = "ª";
-    assertEquals("a", saslPrep.saslPrep(example4, false));
+    assertEquals("a", SaslPrep.saslPrep(example4, true));
     String example5 = "Ⅸ";
-    assertEquals("IX", saslPrep.saslPrep(example5, false));
+    assertEquals("IX", SaslPrep.saslPrep(example5, true));
     String example6 = "\u0007";
     try {
-      saslPrep.saslPrep(example6, false);
+      SaslPrep.saslPrep(example6, true);
       fail();
     } catch (IllegalArgumentException e) {
       assertEquals("Prohibited character \u0007", e.getMessage());
     }
     String example7 = "ا1";
     try {
-      saslPrep.saslPrep(example7, false);
+      SaslPrep.saslPrep(example7, true);
       fail();
     } catch (IllegalArgumentException e) {
       assertEquals("The string contains any RandALCat character but a RandALCat character "
               + "is not the first and the last characters", e.getMessage());
     }
     String supplementary = "𠀀";
-    assertEquals(supplementary, saslPrep.saslPrep(supplementary, false));
+    assertEquals(supplementary, SaslPrep.saslPrep(supplementary, true));
   }
 }
