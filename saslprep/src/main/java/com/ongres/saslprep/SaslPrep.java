653b548fade7c1417f6ca50015ada382401ef08a
bego <begonapm@ongres.com>
2019-07-05 10:38:56 +0200

    Add header with the license and license file

--- saslprep/src/main/java/com/ongres/saslprep/SaslPrep.java
+++ saslprep/src/main/java/com/ongres/saslprep/SaslPrep.java
@@ -1,7 +1,28 @@
+/*
+ * Copyright 2019, OnGres.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 package com.ongres.saslprep;
 
 import com.ongres.stringprep.StringPrep;
-import java.io.IOException;
 import java.nio.CharBuffer;
 import java.text.Normalizer;
 import java.util.ArrayList;
@@ -13,8 +34,11 @@ public class SaslPrep {
 
   /**
  * SaslPrep profile of StringPrep algorithm.
+ * @param value with the value to check and transform
+ * @param storedString boolean to indicate if it's a stored string or a query string
+ * @return String with the result
  */
-  public static String saslPrep(String value, boolean storedString) throws IOException {
+  public static String saslPrep(String value, boolean storedString) {
     List<Integer> valueBuilder = new ArrayList<>();
     //Mapping
     //non-ASCII space characters
