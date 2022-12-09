ad910c03252844ea78ec8cc2fe6db528790a57b3
Jorge Solórzano <jorsol@gmail.com>
2022-12-09 17:47:29 +0000

    Introduce Aggregator POM

--- stringprep/src/main/java/com/ongres/stringprep/StringprepLocator.java
+++ stringprep/src/main/java/com/ongres/stringprep/StringprepLocator.java
@@ -10,14 +10,13 @@ import java.util.ServiceLoader;
 final class StringprepLocator {
 
   private StringprepLocator() {
-    throw new IllegalStateException("Utility class");
+    // Internal utility class
   }
 
   static Profile getProfile(String profileName) {
     for (Profile profile : ServiceLoader.load(Profile.class)) {
       ProfileName annotation = profile.getClass().getDeclaredAnnotation(ProfileName.class);
-      if (annotation != null && annotation.value().equals(profileName) ||
-          profile.getClass().getSimpleName().equals(profileName)) {
+      if (annotation != null && annotation.value().equals(profileName)) {
         return profile;
       }
     }
